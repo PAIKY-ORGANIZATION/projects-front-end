ba1dacd51e8823367a75a5cf84f5e9c22b1f434b
Priapisman677 <expeliamm02@hotmail.com>
2025-07-08 13:17:35 -0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import '@/bootstrap-env';
 
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'next-app-public-bucket.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
+  }
+}
 
 export default nextConfig;
