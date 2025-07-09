5706dc379164635791706b1479eedb0e71d659ba
Priapisman677 <expeliamm02@hotmail.com>
2025-07-08 17:39:24 -0700

    update

--- src/lib/redis-client.ts
+++ src/lib/redis-client.ts
@@ -3,7 +3,7 @@ import {createClient} from 'redis'
 
 const redisClient = new createClient({
     socket: {
-        host: process.env.,
+        host: process.env.REDIS_HOST,
         port: proccess.env.,
     }
 })
