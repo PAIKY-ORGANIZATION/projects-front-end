533922c0c85209ea924c8a912a5d4d0fd8fd439f
Priapisman677 <expeliamm02@hotmail.com>
2025-07-12 07:47:44 -0700

    update

--- src/lib/run-rate-limiter-check.ts
+++ src/lib/run-rate-limiter-check.ts
@@ -1,5 +1,4 @@
 import { cookies, headers } from "next/headers"
-import fs from 'fs'
 
 
 
@@ -15,20 +14,10 @@ export const runRateLimiterCheck = async(): Promise<boolean>=> {
 
     if(!ip) throw new Error('No IP FOUND')
 
-    console.log({ip});
-    
-
-
     //? Hopefully I can get the cookie to handle emails instead of IP addresses.
     if(!rateLimitCookie) {
-        cookieStore.set('RATE_LIMIT_COOKIE', ip, {
-            httpOnly: true,
-        })
+        cookieStore.set('RATE_LIMIT_COOKIE', ip, { httpOnly: true, })
     }
-
-
-    //* logging the IP address in a file:
-    fs.appendFileSync('ip-addresses.txt', `${ip}\n`)
 
     return true
     
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 export const middleware = async (req: NextRequest)=>{
 
-    // @ts-expect-error // ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
+
+    ////  This was going to be used for rate limiting but I decided I will use a unique UUID in a JWT in a cookie.
+    //$ decided to use IP address as a unique identifier amongst all my applications.
+    // @ts-expect-error // ¡
     const ip = req.ip || req.headers.get('x-forwarded-for')?.split(',')[0] || '';
 
     const final = NextResponse.next()
