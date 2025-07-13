ba096c0a85cd6f46b0a571ce22c674035fc43001
Priapisman677 <expeliamm02@hotmail.com>
2025-07-12 20:12:09 -0700

    update

--- src/app/(aws-s3-app)/layout.tsx
+++ src/app/(aws-s3-app)/layout.tsx
@@ -1,5 +1,8 @@
 import Navbar from "@/components/aws-s3-app/Navabar";
 
+export const metadata = {
+  title: 'Post to AWS S3',
+};
 
 export default async function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
 
