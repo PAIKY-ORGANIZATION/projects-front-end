3e90c98646b0eb70fb7f66056644cd97ef442737
Priapisman677 <expeliamm02@hotmail.com>
2025-07-14 13:00:15 -0700

    update

--- src/app/(aws-s3-app)/aws-s3-app/posts/page.tsx
+++ src/app/(aws-s3-app)/aws-s3-app/posts/page.tsx
@@ -1,10 +1,19 @@
 import { getAllPosts } from "@/actions/s3-app/get-all-posts";
 import PostsPage from "@/components/aws-s3-app/PostShow";
-
-
+import {logRequest} from 'req-logger-express'
+import {headers} from 'next/headers'
 
 
 export default async function Posts() {
+
+    const headerStore = await headers()
+    const ip = headerStore.get('ip') || ''
+
+    await logRequest({
+        fileName: 'req.log-s3-app',
+        ip,
+        logInfo: ''
+    })
 
     const posts =  await getAllPosts()
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,29 @@ export const middleware = async (req: NextRequest)=>{
 
 
     ////  This was going to be used for rate limiting but I decided I will use a unique UUID in a JWT in a cookie.
-    //$ decided to use IP address as a unique identifier amongst all my applications.
+    //$ Decided to use IP address as a unique identifier amongst all my applications.
+    //$ In this case it'll have two purposes:
+        //$ 1. Rate limiting
+        //$ 2. Logging
     // @ts-expect-error // ¡
-    const ip = req.ip || req.headers.get('x-forwarded-for')?.split(',')[0] || '';
+    const unparsedIp = req.ip || req.headers.get('x-forwarded-for') || req.headers.get('x-real-ip');
+
+    const ip = unparsedIp?.split(',')[0] || '';
+    
+
+    //! They were not adding this because it also gives you logs when the client's front end loads <Link /> elements and it's misleading and inaccurate. 
+    // const isStaticAsset =
+    //     pathname.startsWith('/_next/') ||        // Static JS, chunks, etc.
+    //     pathname.startsWith('/favicon.ico') ||   // Favicon
+    //     pathname.startsWith('/robots.txt') ||    // Robots
+    //     pathname.startsWith('/sitemap.xml') ||   // Sitemap
+    //     pathname.match(/\.(js|css|png|jpg|jpeg|gif|svg|ico|woff2?)$/); // Other assets
+
+    // if (!isStaticAsset) {
+    //     console.log(`Visited page: ${pathname} from IP: ${ip}`);
+    // }
+
+    
 
     const final = NextResponse.next()
     final.headers.set('ip', ip)
