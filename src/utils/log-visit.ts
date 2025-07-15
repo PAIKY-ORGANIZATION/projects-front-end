2d643bb4496c7f7089b5b1213c89a371f8d44587
Priapisman677 <expeliamm02@hotmail.com>
2025-07-15 07:40:02 -0700

    update

--- src/utils/log-visit.ts
+++ src/utils/log-visit.ts
@@ -3,8 +3,15 @@ import { logRequest } from "req-logger-express"
 
 export const requestLog = async({filePath, logInfo}: {filePath: string, logInfo: string})=>{
 
+    console.log('test 1');
+    
+
     //* If we are in development, avoid writing to the file system because it triggers a lot of hot reloads 
     if(process.env.NODE_ENV === 'production'){
+
+        console.log('test 2');
+        
+
         const headerStore = await headers()
         const ip = headerStore.get('ip') || ''
     
